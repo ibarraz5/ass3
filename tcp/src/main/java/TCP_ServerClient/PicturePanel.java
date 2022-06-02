47c09be9819523aa51ddfce60b6d871b81ead0f2
Ivan <75654991+ibarraz5@users.noreply.github.com>
2022-06-02 00:32:03 -0700

    Update PicturePanel.java

--- tcp/src/main/java/TCP_ServerClient/PicturePanel.java
+++ tcp/src/main/java/TCP_ServerClient/PicturePanel.java
@@ -55,7 +55,7 @@ public class PicturePanel extends JPanel {
 	 */
 	public PicturePanel() {
 		setLayout(new FlowLayout());
-		setSize(1000, 1000);
+		setSize(3000, 5000);
 		labels = new JLabel[0][0];
 
 		state = States.NotStarted;
